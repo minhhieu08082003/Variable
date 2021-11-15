aff0c5e800fbbe23ec0233d63e946f09cdcccb3a
minhhieu08082003 <tranminhhieu08082003@gmail.com>
2021-11-15 12:10:09 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,24 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		/*
+		 * Khai báo biến 
+		 */
+		String firstName = "Nguyen Minh";
+		String lastName = "Hieu";
+		int old = 18;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println("Ho va ten:");
+		System.out.print(firstName + " " + lastName);
+		System.out.println(" Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+		
+		// Khai báo hăng. Hằng phải viết hoa
+		final float PI = 3.14f;
+	}
+
+}
